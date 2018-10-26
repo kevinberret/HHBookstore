ec68ccc51a01eef10830a2b06024cd4ccd8cad18
kevinberret <berret.kevin@gmail.com>
2018-10-26 11:38:31 +0300

    Add REST services to get all books and a book y its id

--- src/main/java/fi/haagahelia/serverprogramming/BookStore/domain/Book.java
+++ src/main/java/fi/haagahelia/serverprogramming/BookStore/domain/Book.java
@@ -9,6 +9,8 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 public class Book {
 	@Id
@@ -16,6 +18,7 @@ public class Book {
 	private long id;
 	
 	@ManyToOne
+	@JsonIgnore
 	@JoinColumn(name = "categoryId")
 	private Category category;
 	
--- src/main/java/fi/haagahelia/serverprogramming/BookStore/web/BookController.java
+++ src/main/java/fi/haagahelia/serverprogramming/BookStore/web/BookController.java
@@ -1,11 +1,16 @@
 package fi.haagahelia.serverprogramming.BookStore.web;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import fi.haagahelia.serverprogramming.BookStore.domain.Book;
 import fi.haagahelia.serverprogramming.BookStore.domain.BookRepository;
@@ -55,4 +60,16 @@ public class BookController {
 		model.addAttribute("categories", categoryRepo.findAll());
 		return "editbook";
 	}
+	
+	// RESTful service to get all books
+	@GetMapping("/books")
+	public @ResponseBody List<Book> bookListRest(){
+		return (List<Book>) bookRepo.findAll();
+	}
+	
+	// RESTful service to get book by id
+	@GetMapping("/book/{id}")
+    public @ResponseBody Optional<Book> findBookRest(@PathVariable("id") Long bookId) {	
+    	return bookRepo.findById(bookId);
+    } 
 }
