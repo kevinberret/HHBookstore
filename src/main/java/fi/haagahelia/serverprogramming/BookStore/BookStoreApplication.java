760fe9037775287cbfe9d47d36fae16bdc263270
kevinberret <berret.kevin@gmail.com>
2018-10-26 13:10:53 +0300

    Add users in database instead of in-memory AND user entity

--- src/main/java/fi/haagahelia/serverprogramming/BookStore/BookStoreApplication.java
+++ src/main/java/fi/haagahelia/serverprogramming/BookStore/BookStoreApplication.java
@@ -11,6 +11,8 @@ import fi.haagahelia.serverprogramming.BookStore.domain.Book;
 import fi.haagahelia.serverprogramming.BookStore.domain.BookRepository;
 import fi.haagahelia.serverprogramming.BookStore.domain.Category;
 import fi.haagahelia.serverprogramming.BookStore.domain.CategoryRepository;
+import fi.haagahelia.serverprogramming.BookStore.domain.User;
+import fi.haagahelia.serverprogramming.BookStore.domain.UserRepository;
 
 @SpringBootApplication
 public class BookStoreApplication {
@@ -20,7 +22,7 @@ public class BookStoreApplication {
 	}
 	
 	@Bean
-	public CommandLineRunner demo(BookRepository bookRepo, CategoryRepository catRepo) {
+	public CommandLineRunner demo(BookRepository bookRepo, CategoryRepository catRepo, UserRepository userRepo) {
 		return args->{
 			Category c1 = new Category("Comédie-ballet");
 			Category c2 = new Category("Roman");
@@ -33,6 +35,12 @@ public class BookStoreApplication {
 			
 			bookRepo.save(b1);
 			bookRepo.save(b2);
+			
+			// Create users: admin/admin user/user
+			User user1 = new User("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER");
+			User user2 = new User("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C", "ADMIN");
+			userRepo.save(user1);
+			userRepo.save(user2);
 		};
 	}
 }
